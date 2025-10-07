6f33c2f77eac07feb80e1d694b3f3ee1e604df12
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-10-08 02:18:11 +0600

    footer layout done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import logo from "../assets/logo.png"
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>i am footer</h1>
-        </div>
-    );
+  return (
+    <div className="bg-[#001931] text-white"> 
+      <footer className="footer sm:footer-horizontal  p-10 w-11/12 mx-auto">
+        <aside>
+         <img className="size-15" src={logo} alt="" />
+          <p>
+            HERO.IO
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
